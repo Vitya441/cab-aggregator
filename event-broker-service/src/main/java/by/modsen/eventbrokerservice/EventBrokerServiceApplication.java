79843a12e65a2be02bfbefe880cbb66153e33c1a
Vitya441 <maksimovviktor554@gmail.com>
2024-10-28 15:50:09 +0300

    Init project

---
+++ event-broker-service/src/main/java/by/modsen/eventbrokerservice/EventBrokerServiceApplication.java
@@ -0,0 +1,13 @@
+package by.modsen.eventbrokerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBrokerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventBrokerServiceApplication.class, args);
+	}
+
+}
---
+++ event-broker-service/src/test/java/by/modsen/eventbrokerservice/EventBrokerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.modsen.eventbrokerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBrokerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
